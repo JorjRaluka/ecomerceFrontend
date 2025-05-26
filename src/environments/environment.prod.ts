e3e3498c5f76df10ba45a922bd65984686219e74
JorjRaluka <raluka.jorj055@gmail.com>
2025-05-26 19:19:05 +0300

    api url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   stripePublishableKey:"pk_test_51QxT7zCBDqiZCtvhYSzZEPDnwAlXRuXt1LLvqW9BBgrcUw18bxtNC1y75j6ABHShn7FkM3nq80ZetPHmbhQz1sdG00aauSeZuo",
-  luv2shopApiUrl:"http://localhost:8080/api"
+  luv2shopApiUrl:"http://springboot-backend.westeurope.azurecontainer.io:8080/api"
 
 };
