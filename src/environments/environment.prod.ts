76d322ff2c6fbed9064186e885d931b52af3c0ae
JorjRaluka <raluka.jorj055@gmail.com>
2025-05-28 12:50:50 +0300

    update env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   stripePublishableKey:"pk_test_51QxT7zCBDqiZCtvhYSzZEPDnwAlXRuXt1LLvqW9BBgrcUw18bxtNC1y75j6ABHShn7FkM3nq80ZetPHmbhQz1sdG00aauSeZuo",
-  luv2shopApiUrl:"http://springboot-backend.westeurope.azurecontainer.io:8080/api"
-
+  luv2shopApiUrl: "https://backend-docker-adc6azhaddg2hybd.germanywestcentral-01.azurewebsites.net/api"
 };
